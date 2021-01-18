210d771e535d262dad0d2025402f2b63cce2bad8
senz <bushiman40@gmail.com>
2021-01-18 05:57:14 +0100

    Programi 1-3

---
+++ Program 2 str.25 (1).cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Za C++ spremni!!!" << endl << "Ne, hvala!";
+	return 0;
+}
---
+++ Program 3 str.30 (1).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Upisi prvi broj: ";
+	cin >> a;
+
+	int b;
+	cout << "Upisi i drugi broj: ";
+	cin >> b;
+
+	int c = a + b;
+
+	cout << "Njihov zbroj je: " << c << endl;
+	return 0;
+}
