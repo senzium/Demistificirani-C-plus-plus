7d2873001c7c82fb44b9c2c381f35088fce891bc
senz <bushiman40@gmail.com>
2021-01-18 07:17:04 +0100

    Programi 4-5
    Upravljanje slijedom izvođenja programa

---
+++ Program 4 str.37 (1).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	// učitajmo prvu stranicu:
+	cout << "Stranica1 = ";
+	double str1;
+	cin >> str1;
+	// učitajmo drugu stranicu:
+	cout << "Stranica2 = ";
+	double str2;
+	cin >> str2;
+	// izračunajmo i ispišimo hipotenuzu
+	double str3 = sqrt(str1 * str1 + str2 * str2);
+	cout << "Stranica3 = " << str3 << endl;
+	return 0;
+}
